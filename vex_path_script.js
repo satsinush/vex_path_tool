2bc10e86ca6143f23992aa65f3ca5d613f965f90
satsinush <140562694+satsinush@users.noreply.github.com>
2024-02-23 20:20:20 -0600

    Added robot
    Now displays an image of the robot
    Added slider to control the position of the robot
    Added buttons to set robot position to each point
    Added a "Play" button to animate the robot on the path
    Added options to set the robot dimensions and speed

--- vex_path_script.js
+++ vex_path_script.js
@@ -7,6 +7,7 @@
 
 var canvas = document.getElementById("canvas");
 var point_list = document.getElementById("point_list");
+var robot_slider = document.getElementById("robot_slider");
 
 var skills_check = document.getElementById("skills_check");
 skills_check.onchange = function(){
@@ -21,20 +22,44 @@ pointHitBox = 10;
 pointSize = 5;
 snapTo = 0;
 skills = false;
+fps = 60;
+play = false;
 
 PX_TO_INCHES = 1/pixelsPerInch;
 INCHES_TO_PX = pixelsPerInch;
 
 class Robot{
-    constructor(x,y,width,length,heading){
+    constructor(x,y,width,length,heading,speed){
         this.x=x;
         this.y=y;
         this.width=width;
         this.length=length;
         this.heading=heading;
+        this.progress = 0;
+        this.speed = speed;
     }
 }
-robot = new Robot(-1000,-1000,15,18,0);
+robot = new Robot(0,0,15,18,90,(Math.PI*3.25*450/60));
+
+lengthInput = document.getElementById("lengthInput");
+widthInput = document.getElementById("widthInput");
+speedInput = document.getElementById("speedInput");
+
+lengthInput.value = robot.length;
+widthInput.value = robot.width;
+speedInput.value = robot.speed;
+
+lengthInput.onchange = function(event){
+    robot.length = parseFloat(this.value);
+    drawCanvas();
+}
+widthInput.onchange = function(event){
+    robot.width = parseFloat(this.value);
+    drawCanvas();
+}
+speedInput.onchange = function(event){
+    robot.speed = parseFloat(this.value);
+}
 
 class Point{
     constructor(x,y){
@@ -73,8 +98,10 @@ function roundToM(num, multiple){
     return((Math.round(num/multiple)*multiple));
 }
 
+var pathLength = 0;
 function updatePoints(){
     if(points.length > 0){
+        pathLength = 0;
         for(i=0; i<points.length; i++){
             //distance/heading toNext
             if(i<points.length-1){
@@ -84,21 +111,30 @@ function updatePoints(){
                 }else{
                     points[i].headingToNext = 270-Math.atan((points[i+1].y-points[i].y)/(points[i+1].x-points[i].x))*180/Math.PI;
                 }
+            }else{
+                points[i].distanceToNext = null;
+                points[i].headingToNext = null;
             }
             //distance/heading fromLast
             if(i > 0){
                 points[i].distanceFromLast = Math.sqrt((points[i].x-points[i-1].x)**2+(points[i].y-points[i-1].y)**2);
+                pathLength += points[i].distanceFromLast;
                 if(points[i].x-points[i-1].x >= 0){
                     points[i].headingFromLast = 90-Math.atan((points[i].y-points[i-1].y)/(points[i].x-points[i-1].x))*180/Math.PI;
                 }else{
                     points[i].headingFromLast = 270-Math.atan((points[i].y-points[i-1].y)/(points[i].x-points[i-1].x))*180/Math.PI;
                 }
+            }else{
+                points[i].distanceFromLast = null;
+                points[i].headingFromLast = null;
             }
         }
-        robot.x = points[0].x;
-        robot.y = points[0].y;
-        robot.heading = points[0].headingToNext;
+        robot_slider.value = (robot.progress/pathLength)*robot_slider.max;
+        if(pathLength < robot.progress){
+            robot.progress = pathLength;
+        }
     }
+    updateRobotPosition();
     drawCanvas();
     updatePointsList();
 }
@@ -201,9 +237,11 @@ function updatePointsList(){
     for(i=0; i<points.length; i++){
         row = point_list.insertRow(point_list.length);
         row.draggable="true";
-        cell = row.insertCell(row.length);
-        cell.innerHTML = i+1;
-        cell.width = "25px";
+        //cell = row.insertCell(row.length);
+        //cell.innerHTML = i+1;
+        //cell.width = "25px";
+        row.insertCell(row.length).appendChild(
+            createInput("goTo", i, "button", (i+1).toString(), "50px",robotGoTo));
         row.insertCell(row.length).appendChild(
             createInput("xInput", i,"number",points[i].x.toFixed(2),"50px", updateX));
         row.insertCell(row.length).appendChild(
@@ -217,7 +255,7 @@ function updatePointsList(){
             row.insertCell();
             row.insertCell();
         }
-        if(i < points.length-1){
+        /*if(i < points.length-1){
             row.insertCell(row.length).appendChild(
                 createInput("distanceNextInput", i,"number",points[i].distanceToNext.toFixed(2),"50px", updateDistanceToNext));
             row.insertCell(row.length).appendChild(
@@ -225,7 +263,7 @@ function updatePointsList(){
         }else{
             row.insertCell();
             row.insertCell();
-        }
+        }*/
         row.insertCell(row.length).appendChild(
             createInput("insertButton", i, "button","Insert","50px",insertButton));
         row.insertCell(row.length).appendChild(
@@ -275,13 +313,36 @@ function drawCanvas(){
     ctx.globalAlpha = 1;
 
     if(points.length > 0){
-        //draw robot
+        //draw lines 
         ctx.beginPath();
+        ctx.moveTo(points[0].getPx_X(), points[0].getPx_Y());
+        for(i=0; i<points.length; i++){
+            x = points[i].x;
+            y = points[i].y;
+            ctx.lineTo(inchesToPx_X(x), inchesToPx_Y(y));
+            //draw arrow
+            if(points[i].headingFromLast != null){
+                midX = (points[i].x+points[i-1].x)/2
+                midY = (points[i].y+points[i-1].y)/2
+                vAngle = 360+(points[i].headingFromLast)-90;
+                arrowSize = 10;
+                ctx.moveTo(inchesToPx_X(midX),inchesToPx_Y(midY));
+                ctx.lineTo(inchesToPx_X(midX)+(arrowSize)*Math.cos((vAngle+150)*(Math.PI/180)), inchesToPx_Y(midY)+(arrowSize)*Math.sin((vAngle+150)*(Math.PI/180)));
+                ctx.moveTo(inchesToPx_X(midX),inchesToPx_Y(midY));
+                ctx.lineTo(inchesToPx_X(midX)+(arrowSize)*Math.cos((vAngle-150)*(Math.PI/180)), inchesToPx_Y(midY)+(arrowSize)*Math.sin((vAngle-150)*(Math.PI/180)));
+                ctx.moveTo(inchesToPx_X(x), inchesToPx_Y(y));
+            }
+        }
+        ctx.stroke();
+        ctx.closePath();
+
+        //draw robot
         angle = 360+(robot.heading)-90;
         angle = -robot.heading+90;
-        print(angle);
         dAngle = Math.atan(robot.width/robot.length)*(180/Math.PI)
         d = Math.sqrt((robot.width/2)**2+(robot.length/2)**2);
+        /*
+        ctx.beginPath();
         ctx.moveTo(
             inchesToPx_X(robot.x+d*Math.cos((angle+dAngle)*(Math.PI/180))),
             inchesToPx_Y(robot.y+d*Math.sin((angle+dAngle)*(Math.PI/180))));
@@ -298,29 +359,15 @@ function drawCanvas(){
             inchesToPx_X(robot.x+d*Math.cos((angle+dAngle*1)*(Math.PI/180))),
             inchesToPx_Y(robot.y+d*Math.sin((angle+dAngle*1)*(Math.PI/180))));
         ctx.stroke();
-        ctx.closePath(); 
-
-        //draw line 
-        ctx.beginPath();
-        ctx.moveTo(points[0].getPx_X(), points[0].getPx_Y());
-        for(i=0; i<points.length; i++){
-            x = points[i].x;
-            y = points[i].y;
-            ctx.lineTo(inchesToPx_X(x), inchesToPx_Y(y));
-            if(points[i].headingFromLast != null){
-                midX = (points[i].x+points[i-1].x)/2
-                midY = (points[i].y+points[i-1].y)/2
-                vAngle = 360+(points[i].headingFromLast)-90;
-                arrowSize = 10;
-                ctx.moveTo(inchesToPx_X(midX),inchesToPx_Y(midY));
-                ctx.lineTo(inchesToPx_X(midX)+(arrowSize)*Math.cos((vAngle+150)*(Math.PI/180)), inchesToPx_Y(midY)+(arrowSize)*Math.sin((vAngle+150)*(Math.PI/180)));
-                ctx.moveTo(inchesToPx_X(midX),inchesToPx_Y(midY));
-                ctx.lineTo(inchesToPx_X(midX)+(arrowSize)*Math.cos((vAngle-150)*(Math.PI/180)), inchesToPx_Y(midY)+(arrowSize)*Math.sin((vAngle-150)*(Math.PI/180)));
-                ctx.moveTo(inchesToPx_X(x), inchesToPx_Y(y));
-            }
-        }
-        ctx.stroke();
         ctx.closePath();
+        */
+
+        image = document.getElementById("robot_image");
+        ctx.translate(inchesToPx_X(robot.x), inchesToPx_Y(robot.y));
+        ctx.rotate(-(angle)*(Math.PI/180));
+        ctx.drawImage(image, -robot.length/2*pixelsPerInch, -robot.width/2*pixelsPerInch, robot.length*pixelsPerInch, robot.width*pixelsPerInch);
+        ctx.rotate((angle)*(Math.PI/180));
+        ctx.translate(-inchesToPx_X(robot.x), -inchesToPx_Y(robot.y));
 
         //draw points
         for(i=0; i<points.length; i++){
@@ -343,6 +390,7 @@ function drawCanvas(){
 }
 
 function getTouching(x,y){
+    wasTouching = touchingPoint;
     for(i=points.length-1; i>=0; i--){
         if(Math.sqrt(((x-points[i].x))**2+(y-points[i].y)**2) < pointHitBox*PX_TO_INCHES){
             touchingPoint = i;
@@ -361,33 +409,32 @@ mouseDown = false;
 selectedPoint = null;
 touchingPoint = null;
 canvas.onmousedown = function(event){
-    mouseDown = true;
     x=pxToInches_X(event.offsetX);
     y=pxToInches_Y(event.offsetY);
     selectedPoint = getTouching(x,y);
     if(event.button == 0){
+        mouseDown = true;
         if(selectedPoint == null){
             newPoint(snap(x),snap(y));
             touchingPoint = points.length-1;
             selectedPoint = points.length-1;
-        }else{
+            updatePoints();
         }
     }
     //right click
     if(event.button == 2){
         if(selectedPoint != null){
             deletePoint(selectedPoint);
-            getTouching();
+            getTouching(x,y);
+            updatePoints();
         }
     }
-    updatePoints();
 }
 
 canvas.onmouseup = function(event){
     x=pxToInches_X(event.offsetX);
     y=pxToInches_Y(event.offsetY);
     mouseDown = false;
-    updatePoints();
 }
 
 canvas.onmousemove = function(event){
@@ -396,15 +443,97 @@ canvas.onmousemove = function(event){
     if(mouseDown && selectedPoint != null){
         points[selectedPoint].x = snap(x);
         points[selectedPoint].y = snap(y);
+        updatePoints();
     }else{
+        lastTouching = touchingPoint;
         getTouching(x,y);
+        if(touchingPoint != lastTouching){
+            drawCanvas();
+        }
     }
-    updatePoints();
+}
+
+function vexHeadingtoTrigAngle(input){
+    return(-input+90);
+}
+robot_slider.oninput = function(event){
+    robot.progress = pathLength*(this.value/robot_slider.max);
+    updateRobotPosition();
+    drawCanvas();
+}
+
+function robotGoTo(v,index){
+    total = 0
+    for(i=0; i<index; i++){
+        if(i < points.length-1){
+            total += points[i].distanceToNext;
+        }
+    }
+    robot.progress = total;
+    robot_slider.value = (robot.progress/pathLength)*robot_slider.max;
+    updateRobotPosition();
+    drawCanvas();
+}
+
+function updateRobotPosition(){
+    total = robot.progress;
+    index = 0;
+    for(i=0; i<points.length; i++){
+        if(total >= points[i].distanceToNext){
+            total -= points[i].distanceToNext;
+        }else{
+            index = i;
+            break;
+        }
+    }
+    if(points.length > 0){
+        if(robot.progress >= pathLength && points.length > 1){
+            index = points.length-2;
+            total = points[index].distanceToNext;
+        }
+        if(! isNaN(points[index].headingToNext)){
+            robot.x = points[index].x + total*Math.cos(vexHeadingtoTrigAngle(points[index].headingToNext)*Math.PI/180);
+            robot.y = points[index].y + total*Math.sin(vexHeadingtoTrigAngle(points[index].headingToNext)*Math.PI/180);
+        }else{
+            robot.x = points[index].x;
+            robot.y = points[index].y;
+        }        
+        robot.heading = points[index].headingToNext;
+    }
+}
+
+function animateRobot(){
+    if(play){
+        if(robot.progress < pathLength){
+            robot.progress += robot.speed/fps;
+        }else{
+            play = !play;
+            if(play){playButton.value = "Stop";
+            }else{playButton.value = "Play";}
+        }
+        robot_slider.value = (robot.progress/pathLength)*robot_slider.max;
+        updateRobotPosition();
+        drawCanvas();
+    }
+}
+playButton = document.getElementById("playButton");
+playButton.onclick = function (){
+    if(play){
+        play = false;
+        playButton.value = "Play";
+    }else{
+        if(robot.progress >= pathLength){
+            robot.progress = 0;
+        }
+        play = true;
+        playButton.value = "Stop";
+    }
 }
 
 function exportData(){
     content = "";
     content+=skills.toString()+","+snapTo.toString()+"\n";
+    content+=robot.length.toString()+","+robot.width.toString()+","+robot.speed.toString()+"\n";
     for(i=0; i<points.length; i++){
         content += points[i].x.toString() + "," + points[i].y.toString() +"\n";
     }
@@ -417,54 +546,67 @@ function exportData(){
     URL.revokeObjectURL(link.href);
 }
 function importData(){
-    if(confirm("Importing a file will clear all existing points."))
-    input = document.createElement('input');
-    input.accept = ".txt"
-    input.type = 'file';
-    input.onchange = function(){
-        //load file
-        file = input.files[0];
-        const reader = new FileReader();
-        reader.onload = function() {
-            contents = reader.result;
-            //parse contents
-            token = "";
-            tokenNum = 0;
-            x=0;
-            y=0;
-            pList = []
-            for(i=0; i<contents.length; i++){
-                char = contents[i];
-                if(char == "," || char=="\n"){
-                    if(tokenNum==0){
-                        skills = (token == "true");
-                        skills_check.checked = skills;
-                    }
-                    if(tokenNum==1){
-                        snapTo = parseFloat(token);
-                        snapInput.value = snapTo;
-                    }
-                    if(tokenNum >= 2){
-                        if((tokenNum-2)%2==0){
-                            //print([tokenNum,token]);
-                            x = parseFloat(token);
-                        }else{
-                            y = parseFloat(token);
-                            pList.push(new Point(x,y));
+    if(confirm("Importing a file will clear all existing points.")){
+        input = document.createElement('input');
+        input.accept = ".txt"
+        input.type = 'file';
+        input.onchange = function(){
+            //load file
+            file = input.files[0];
+            const reader = new FileReader();
+            reader.onload = function() {
+                contents = reader.result;
+                //parse contents
+                token = "";
+                tokenNum = 0;
+                x=0;
+                y=0;
+                pList = []
+                for(i=0; i<contents.length; i++){
+                    char = contents[i];
+                    if(char == "," || char=="\n"){
+                        if(tokenNum==0){
+                            skills = (token == "true");
+                            skills_check.checked = skills;
                         }
+                        if(tokenNum==1){
+                            snapTo = parseFloat(token);
+                            snapInput.value = snapTo;
+                        }
+                        if(tokenNum==2){
+                            robot.length = parseFloat(token);
+                            lengthInput.value = robot.length;
+                        }
+                        if(tokenNum==3){
+                            robot.width = parseFloat(token);
+                            widthInput.value = robot.width;
+                        }
+                        if(tokenNum==4){
+                            robot.speed = parseFloat(token);
+                            speedInput.value = robot.speed;
+                        }
+                        if(tokenNum >= 5){
+                            if((tokenNum-5)%2==0){
+                                x = parseFloat(token);
+                            }else{
+                                y = parseFloat(token);
+                                pList.push(new Point(x,y));
+                            }
+                        }
+                        tokenNum++;
+                        token = "";
+                    }else{
+                        token += char;
                     }
-                    tokenNum++;
-                    token = "";
-                }else{
-                    token += char;
                 }
-            }
-            points = Array.from(pList);
-            updatePoints();
-        };
-        reader.readAsText(file);
+                points = Array.from(pList);
+                updatePoints();
+            };
+            reader.readAsText(file);
+        }
+        input.click();
     }
-    input.click();
 }
 
 updatePoints();
+setInterval(animateRobot,(1000/fps));
